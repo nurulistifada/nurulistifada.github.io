667ba24de50c7dcc292d20e26cbccb2444751519
nurulistifada <nurulistifada1212@gmail.com>
2022-09-06 14:32:10 +0700

    text html

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <text><text>
+    </head>
+    <body>
+
+        <header> Hello World! </header>
+
+        <article> </article>
+
+    <footer> Thank You! </footer>
+
+    </body>
+</html>
